8725db725f3fc51deb8e5524894084f6f25c8d23
sharnam shah <23cs089@charusat.edu.in>
2025-07-11 15:13:03 +0530

    updated produtcard

--- GREENCART/client/src/components/ProductCard.jsx
+++ GREENCART/client/src/components/ProductCard.jsx
@@ -29,7 +29,7 @@ const ProductCard = ({ product }) => {
        
 
 
-<div className="flex items-end justify-between mt-3">
+<div className="flex flex-col gap-2 mt-3">
   <div className="md:text-xl text-base font-medium text-green-600">
     {currency}
     {(() => {
@@ -47,23 +47,20 @@ const ProductCard = ({ product }) => {
 
   <div onClick={(e) => { e.stopPropagation(); }} className="text-green-600">
     {!cartItems[product._id] ? (
-      <button className="flex items-center justify-center gap-1 bg-green-100 border border-green-300 md:w-[80px] w-[64px] h-[34px] rounded text-green-600 font-medium cursor-pointer" onClick={() => addToCart(product._id)} >
+      <button className="flex items-center justify-center gap-1 bg-green-100 border border-green-300 w-full h-[34px] rounded text-green-600 font-medium cursor-pointer" onClick={() => addToCart(product._id)} >
         <img src={assets.cart_icon} alt='cart icon' />
         Add
       </button>
     ) : (
-      <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-indigo-500/25 rounded select-none">
-        <button onClick={() => { removeFromCart(product._id) }} className="cursor-pointer text-md px-2 h-full" >
-          -
-        </button>
+      <div className="flex items-center justify-between w-full h-[34px] bg-indigo-500/25 rounded select-none px-2">
+        <button onClick={() => { removeFromCart(product._id) }} className="cursor-pointer text-md" >-</button>
         <span className="w-5 text-center">{cartItems[product._id]}</span>
-        <button onClick={() => { addToCart(product._id) }} className="cursor-pointer text-md px-2 h-full" >
-          +
-        </button>
+        <button onClick={() => { addToCart(product._id) }} className="cursor-pointer text-md" >+</button>
       </div>
     )}
   </div>
 </div>
+
 
       </div>
     </div>

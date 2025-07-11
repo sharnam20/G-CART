3b35900ce0f2466cef37965b830d8512cae5d326
sharnam shah <23cs089@charusat.edu.in>
2025-07-11 15:06:41 +0530

    upadated allproduct

--- GREENCART/client/src/pages/AllProducts.jsx
+++ GREENCART/client/src/pages/AllProducts.jsx
@@ -19,7 +19,8 @@ const AllProducts = () => {
   }, [products, serchQuery]);
 
   return (
-    <div className="mt-16 flex flex-col">
+    <div className="pt-[70px] px-4 md:px-8 flex flex-col">
+
       <div className="flex flex-col items-end w-max">
         <p className="text-2xl font-medium uppercase">All Products</p>
         <div className="w-16 h-0.5 bg-green-500 rounded-full"></div>
